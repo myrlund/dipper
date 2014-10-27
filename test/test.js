e8fac64b999f717cf1af988d2e68ba3fb3f85ec1
Jonas Myrlund <myrlund@gmail.com>
2014-10-27 19:26:09 +0100

    Removed useless test.

--- test/test.js
+++ test/test.js
@@ -8,31 +8,11 @@ describe('Dipper', function () {
     describe('createApplication', function () {
         it('rejects empty configs', function (done) {
             var app = dipper.createApplication({
-                serviceConfig: []
+                config: []
             });
 
             app.catch(function (e) {
                 expect(e).to.be.ok;
-                done();
-            });
-        });
-
-        it('loads and sets up provided packages', function (done) {
-            var packages = ['package1'];
-            var fakeServiceRegistry = { service: 'registry' };
-
-            var _dipper = new dipper.Dipper({
-                packageConfig: packages
-            });
-            _dipper.serviceRegistry = fakeServiceRegistry;
-
-            var loadStub =  sinon.stub(_dipper, 'loadPackages').returns(Q.resolve());
-            var setupStub =  sinon.stub(_dipper, 'setupServices').returns(Q.resolve());
-
-            _dipper.bootstrap().then(function (serviceRegistry) {
-                expect(serviceRegistry).to.eql(fakeServiceRegistry);
-                expect(loadStub.calledOnce).to.be.true;
-                expect(setupStub.calledOnce).to.be.true;
                 done();
             });
         });
